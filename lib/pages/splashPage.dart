258e7c680ef68f824f53ad969bcc42a1c2c75dee
Hubaib19 <hubaib0019@gmail.com>
2023-12-07 22:30:00 +0530

    updated

--- lib/pages/splashPage.dart
+++ lib/pages/splashPage.dart
@@ -7,10 +7,27 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          Image.asset('lib/images/logo-removebg-preview.png')
-        ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+              children:[ Image.asset(
+            'lib/images/logo-removebg-preview.png',
+            height: 240,
+          )),
+        ),
+        const SizedBox(height: 48),
+        const Text(
+          'Just Do it',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        const SizedBox(height: 24),
+        const Text(
+          'Brand new sneakers and custom kicks made with premium quality',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
